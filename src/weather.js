fae17bcf83c8646d2484f7518d3eaec579769c47
NYOLIE <nyolie@yahoo.com>
2025-06-22 20:38:54 -0400

    renamed to index.html and modified css and js file

--- src/weather.js
+++ src/weather.js
@@ -1,38 +1,38 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+let now = new Date();
+let currentDate = document.querySelector("#current-date");
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
 
-let city = prompt("Enter a city").toLowerCase();
+currentDate.innerHTML = `${day}, ${hours}:${minutes}`;
 
-if (weather.hasOwnProperty(city)) {
-  let tempC = Math.round(weather[city].temp);
-  let tempF = Math.round((weather[city].temp * 9) / 5 + 32);
-  let humidity = weather[city].humidity;
+function search(event) {
+  event.preventDefault();
+  let apiKey = "a6ao5dab2743f4304bbcd06edecf3td9";
+  let enterCityElement = document.querySelector("#enter-city");
+  let city = enterCityElement.value;
 
-  alert(
-    `It is currently ${tempC}°C (${tempF}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let cityForm = document.querySelector("#city-form");
+cityForm.addEventListener("submit", search);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let yourCityElement = document.querySelector("#your-city");
+  yourCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
 }
